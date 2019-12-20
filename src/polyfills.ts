0a98f978ec92f124f60924c044f5a16ffa84a4ac
Michael Prentice <splaktar@gmail.com>
2019-12-20 09:49:31 -0500

    fix: lint issues introduced by scully schematic

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,10 +63,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 
 /***************************************************************************************************
-  
 * SCULLY IMPORTS
-  
 */
-  
-// tslint:disable-next-line: align 
 import 'zone.js/dist/task-tracking';
